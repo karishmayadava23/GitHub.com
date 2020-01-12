b25efb1ba2316d7165de00384aec438075d5f429
karishmayadava23 <59534066+karishmayadava23@users.noreply.github.com>
2020-01-13 01:02:48 +0530

    Update LoginPageTest.java
    I made change to pull

--- src/test/java/com/crm/qa/test/LoginPageTest.java
+++ src/test/java/com/crm/qa/test/LoginPageTest.java
@@ -4,7 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
+/**Karry/
 import com.crm.qa.base.TestBase;
 import com.crm.qa.pages.FirstPage;
 import com.crm.qa.pages.HomePage;
